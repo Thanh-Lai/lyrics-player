052b297895045bf3045edd21580846efdd2c3bb8
Thanh-Lai <tlai1122@gmail.com>
2021-02-19 17:44:41 -0500

    fixed profile image

--- src/client/components/LoggingButton.js
+++ src/client/components/LoggingButton.js
@@ -7,11 +7,12 @@ export default function LogginButton({ handleClick }) {
     const storage = JSON.parse(localStorage.getItem(`Spotify_${platform.name}`));
     const profile = storage ? storage.profileInfo : {};
     if (Object.keys(profile).length) {
+        const image = profile.images.length ? profile.images[0].url : defaultProfile;
         return (
             <li>
                 <button type="button" id="loginBtn" onClick={handleClick.out}>Log Out</button>
                 <div id="user">
-                    <img id="profilePic" alt="profilePic" src={profile.images[0].url || defaultProfile} />
+                    <img id="profilePic" alt="profilePic" src={image} />
                     {profile.display_name}
                 </div>
             </li>
